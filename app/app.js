b9f1678a9fbde269a8882a9c16ab7004eb655d0c
Horatiu Jeflea <horatiu@yolobe.com>
2019-10-21 16:53:27 +0300

    Config NPM

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
-console.log("abc");
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/status', function (req, res) {
+    const pjson = require('../package.json');
+    res.send({
+        version: pjson.version,
+        status: "up"
+    });
+});
+
+app.listen(port, () => console.log(`User likes app listening on port ${port}!`));
