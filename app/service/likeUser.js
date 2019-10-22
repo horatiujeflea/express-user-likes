e33f31536e83480a10ad53e5066b9491a8c14329
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2019-10-22 15:22:06 +0300

    Handle exceptions for user likes

--- app/service/likeUser.js
+++ app/service/likeUser.js
@@ -1,21 +1,33 @@
-let { ValidationError } = require('../error/ValidationError');
+let {ValidationError} = require('../error/ValidationError');
 
 const likeUser = async (userId, knex) => {
     let loggedInUserId = 1;
     let toUserId = parseInt(userId);
 
-    let insertQ = knex('user_like')
-        .insert([{from_user: loggedInUserId, to_user: toUserId}]);
-    let insertIgnoreQ = knex.raw(insertQ.toQuery())
-        + " ON DUPLICATE KEY DO NOTHING";
-
-    await insertIgnoreQ;
-
-    return {
+    const successResp = {
         from: loggedInUserId,
         to: toUserId,
         state: "successful"
     };
+
+    try {
+        const insertQ = knex('user_like')
+            .insert([{from_user: loggedInUserId, to_user: toUserId}]);
+        await insertQ;
+
+        return successResp;
+    } catch (e) {
+        switch (e.constraint) {
+            case 'from_user_cannot_be_equal_to_to_user_chk':
+                throw new ValidationError('You cannot like yourself');
+            case 'user_like_to_user_fkey':
+                throw new ValidationError('User id does not exist');
+            case 'user_like_pkey':
+                return successResp;
+            default:
+                throw new Error(e);
+        }
+    }
 };
 
 module.exports = {
