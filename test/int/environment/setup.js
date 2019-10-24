fbb65b13c1e706c1664eaceb87ef379a0f753f08
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2019-10-24 10:02:36 +0300

    Add integration tests

--- test/int/environment/setup.js
+++ test/int/environment/setup.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const supertest = require('supertest');
-
-// require('../../../app/app');
-
-const request = supertest(`http://localhost:${process.env.PORT}`);
-
-module.exports = {
-    request
-};
-
 console.log("*** SETTING UP ***");
 
 // create a new schema from db/schema.sql
 // run db/db-data/int/data.sql
+
+require('dotenv').config();
+const supertest = require('supertest');
+
+const request = supertest(`http://localhost:${process.env.PORT}`);
+
+console.log("*** FINISHED SETTING UP ***");
+
+module.exports = {
+    request
+};
--- test/int/environment/teardown.js
+++ test/int/environment/teardown.js
@@ -1,3 +1,5 @@
 console.log("*** TEARING DOWN ***");
 
 // drop schema created from db/schema.sql
+
+console.log("*** FINISHED TEARING DOWN ***");
