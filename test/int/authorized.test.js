07051e1719cf090ca1f4d59fef7752ac7b32d990
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2019-10-24 11:54:04 +0300

    Add integration tests

--- test/int/authorized.test.js
+++ test/int/authorized.test.js
@@ -1,58 +1,47 @@
-const setCookie = require('set-cookie-parser');
-
 const { request } = require('./environment/supertest');
+
+const userName = 'bobdylan' + getRandomInt(1000);
+const password = 'passabcd12';
 
 
 describe("Authorized Integration Suite", () => {
-    const userName = "bobdylan" + getRandomInt(1000);
-    let token;
 
-    beforeAll(() => {
-        console.log("regA");
-        return new Promise(resolve => {
-            const promise = request.post('/signup')
-                .send({username: userName, password: 'passabcd12'})
-                .expect('Content-Type', /json/)
-                .expect(200);
-            console.log("registered");
-            resolve(promise);
-        });
+    beforeAll(async (done) => {
+        const response = await request.post('/signup')
+            .send({username: userName, password: password})
+            .expect('Content-Type', /json/)
+            .expect(200);
+        done();
     });
 
     afterAll(() => {
         // delete `userName`
     });
 
-    beforeEach(() => {
-        console.log("logA");
-        return new Promise(resolve => {
-            const promise = request.post('/login')
-                .send({username: userName, password: 'passabcd12'})
-                .expect(200);
-            console.log("login");
-            resolve(promise);
-        });
-    });
-
-
     test('Me should return logged in user info', async () => {
-        console.log("test");
-        console.log("abc");
-        const response = await request.get('/me');
-        console.log(response.body);
+        const cookieHeader = await login(userName, password);
 
+        const response = await request
+            .get('/me')
+            .set('Cookie', cookieHeader);
 
-        // const response = await request.get('/me')
-        //     .expect(200);
-        //
-        // expect(response.body).toEqual(
-        //     {"likes": "1", "username": "johndoe1@gmail.com"}
-        // );
+        expect(response.body).toEqual(
+            { username: userName }
+        );
     });
 
+    const login = async () => {
+        const loginResp = await request.post('/login')
+            .send({username: userName, password: password})
+            .expect(200);
+
+        return loginResp.header['set-cookie'];
+    };
 
 });
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+
