a7b7efe56ad8974561bef86745124d8b5a638392
Horatiu Jeflea <horatiu.jeflea@gmail.com>
2019-11-04 07:57:50 +0200

    Refactor

--- app/app.js
+++ app/app.js
@@ -1,6 +1,28 @@
-const { knex, app } = require('./config/app-config');
+require('dotenv').config();
 
-const routes = require('./route')(knex);
-app.use(routes);
+const container = require('./ioc/container');
+
+const knex = require('knex')({
+    client: 'pg',
+    version: '11.5',
+    connection: {
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : process.env.DB_DATABASE
+    }
+});
+
+container.knex = knex;
+
+const express = require('express');
+const app = express();
+
+container.app = app;
+
+app.use(express.json());
+app.use(require('body-parser').json());
+app.use(require('cookie-parser')());
+app.use(require('./route')(knex));
 
 app.listen(process.env.PORT, () => console.log(`User likes app listening on port ${process.env.PORT}!`));
